be273bc87023efb529fce98fb0f87680c80bb281
Michael Biebl <biebl@debian.org>
2009-04-21 01:12:56 +0200

    Update autogen.sh for configure.ac

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 REQUIRED_AUTOMAKE_VERSION=1.7
 PKG_NAME=NetworkManager-openconnect
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
